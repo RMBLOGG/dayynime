a5d73f3602776cedfee70586bc743e7f2e213ec9
Dayy <160155109+RMBLOGG@users.noreply.github.com>
2026-05-15 04:58:12 +0700

    Add files via upload

--- static/sw.js
+++ static/sw.js
@@ -1,10 +1,10 @@
-/* ── Adsterra Multitag ────────────────────────────── */
+/* ── Monetag Multitag ─────────────────────────────── */
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11007262
+    "domain": "3nbf4.com",
+    "zoneId": 11007289
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
 
 /* =====================================================
    DayyNime — Service Worker
