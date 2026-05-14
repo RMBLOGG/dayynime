2d817207b6a7bf5a79c62c663d00acc7573a6be7
Dayy <160155109+RMBLOGG@users.noreply.github.com>
2026-05-15 04:48:05 +0700

    Add files via upload

--- static/sw.js
+++ static/sw.js
@@ -1,3 +1,11 @@
+/* ── Adsterra Multitag ────────────────────────────── */
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11007262
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
 /* =====================================================
    DayyNime — Service Worker
    Cache shell statis, fallback offline
